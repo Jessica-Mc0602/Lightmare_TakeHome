52626840ce160b52e92630a190571c734c14a116
Jessica-Mc0602 <jessicalaramc@gmail.com>
2024-08-05 23:12:09 +1000

    folder movement

--- Packages/LightmareTakeHome/Runtime/Scripts/HoverEffect.cs
+++ Packages/LightmareTakeHome/Runtime/Scripts/HoverEffect.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class HoverEffect : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
 {
     Vector3 orginalScale;
+    AudioSource audioSource;
     
     void Start()
     {
         orginalScale = transform.localScale; //Store the orginal scale of the panel
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            Debug.LogError("AudioSource component not found on " + gameObject.name);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.localScale = orginalScale * 1.1f; //Increase the size by 10%
+        if (audioSource != null)
+            audioSource.Play();
     }
 
     public void OnPointerExit(PointerEventData eventData)
